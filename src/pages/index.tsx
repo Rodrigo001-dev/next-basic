fb2c3dc567b797193d3884f695a9df0169d53f36
Rodrigo <rodrigorael53@gmail.com>
2022-05-19 16:43:18 -0300

    using the server side of next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,35 @@
-export default function Home() {
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import { GetServerSideProps } from 'next';
+
+type Repository = {
+  name: string
+};
+
+export default function Home({ data, date }) {
+
   return (
-    <h1>Hello</h1>
+    <>
+    <h1>{date}</h1>
+      <ul>
+        {data?.map(repo => {
+          return (
+            <li key={repo.name}>{repo.name}</li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await axios.get('https://api.github.com/users/Rodrigo001-de/repos')
+
+  return {
+    props: {
+      data: response.data,
+      date: new Date().toISOString(),
+    }
+  };
+};
