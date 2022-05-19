c5b15033db3f840b0b4b77ed9313086fb563d439
Rodrigo <rodrigorael53@gmail.com>
2022-05-19 15:46:19 -0300

    finishing the settings for react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import { QueryClientProvider } from 'react-query';
+import { queryClient } from '../services/queryClient';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp;
